c77df7143baea1318fd5e46190cbe91993ed256e
Harman <harmanradix@gmail.com>
2018-05-13 16:32:12 -0700

    feat: ModelForm now has a props interface

--- src/ModelForm.tsx
+++ src/ModelForm.tsx
@@ -3,8 +3,13 @@ import * as React from 'react';
 import FormViewModel from './FormViewModel';
 export const ModelFormContext = React.createContext<FormViewModel>(null);
 
+export interface ModelFormProps {
+  model?: FormViewModel;
+}
+
+
 @observer
-export default class ModelForm extends React.Component<any, any> {
+export default class ModelForm extends React.Component<ModelFormProps, any> {
   render() {
     return <ModelFormContext.Provider value={this.props.model}>
       {this.props.children}
