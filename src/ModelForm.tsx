361d9c7518e099f13db12d9ffd8b6d2c0573e207
Harman <harmanradix@gmail.com>
2018-05-13 22:02:39 -0700

    0.1.0 release

--- src/ModelForm.tsx
+++ src/ModelForm.tsx
@@ -1,7 +1,7 @@
 import {observer} from 'mobx-react';
 import * as React from 'react';
 import FormViewModel from './FormViewModel';
-export const ModelFormContext = React.createContext<ModelFormProps>(null);
+export const ModelFormContext = React.createContext<FormViewModel>(null);
 
 export interface ModelFormProps {
   model?: FormViewModel;
@@ -11,8 +11,8 @@ export interface ModelFormProps {
 @observer
 export default class ModelForm extends React.Component<ModelFormProps, any> {
   render() {
-    let {children, ...propsToSend} = this.props;
-    return <ModelFormContext.Provider value={{model: this.props.model, ...propsToSend}}>
+    const {children, model} = this.props;
+    return <ModelFormContext.Provider value={model}>
       {children}
     </ModelFormContext.Provider>
   }
