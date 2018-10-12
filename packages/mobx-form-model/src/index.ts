1ad1e48c61665120ce8e4f7ae6a6826700a1f431
Harman <5001432+hrgui@users.noreply.github.com>
2018-10-11 22:30:23 -0700

    chore: storybook works now

--- packages/mobx-form-model/src/index.ts
+++ packages/mobx-form-model/src/index.ts
@@ -1,7 +1,6 @@
 export { default as Field } from './Field';
 export {
   default as ModelForm,
-  ModelFormContext,
   ModelFormProps
 } from './ModelForm';
 export { default as FormViewModel } from './FormViewModel';
